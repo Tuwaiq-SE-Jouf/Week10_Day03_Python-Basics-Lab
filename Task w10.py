d19dc5c21ca8a815762cecf6742a4f0168e33ee1
Meshal alanazi <92294577+Meshal552@users.noreply.github.com>
2021-12-14 22:46:50 +0300

    Add files via upload

---
+++ Task w10.py
@@ -0,0 +1,37 @@
+ # Task 1: add two numbers
+# num = int(2)
+# num1 = int(7)
+# sum = (num+num1)
+# print(f"The Total:{+sum }")
+# or
+
+# def sum(a, b):
+#     print(a+b)
+#
+# sum(5,6)
+
+
+# Task 2: input add two numbers
+# a = int(input("Enter the  first number: "))
+# b = int(input("Enter the scound number: "))
+# sum = (a+b)
+# print(sum)
+# def sum(a, b) :
+#   input(f"Enter the first nomber:")
+
+
+# Task 3: Calculate prime numbers
+
+
+
+
+
+# Task 4:
+
+# name = input ("Enter you name: ").lower()
+# gpa = float(input("Enter the GPA (If you do not know the rate, enter 0): "))
+# gpa = float(gpa)
+# if name ==name and   gpa >0:
+#     print(f"the Name: {name} the GPA: {gpa}")
+# else:
+#     print(f"the Name: {name} the GPA:0")
